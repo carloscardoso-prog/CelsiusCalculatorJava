2f773b40650f8d29ccbeaef126ac2aadf246c5ca
CarlosCardoso-prog <carloseduardocardsilva@gmail.com>
2021-01-18 00:48:29 -0300

    Java Cº to Fº Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,72 +4,78 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        printMenu();
+        int type;
 
-        int type = new Scanner(System.in).nextInt();
-        int value = valueReceiver();
+        do{
+            printMenu();
 
-        switch(type){
-            case 1:
+            type = new Scanner(System.in).nextInt();
+            int value = valueReceiver();
+
+            switch (type) {
+                case 1:
+
                     result(convertFtoC(value), type);
-                break;
+                    break;
+                case 2:
 
-            case 2:
-                result(convertCtoF(value), type);
-                break;
+                    result(convertCtoF(value), type);
+                    break;
+                default:
 
-            case 3:
-                System.out.println("Bye bye!");
-                System.exit(0);
-                break;
+                    System.out.println("Sorry, couldn't understand");
+                    continue;
 
-            default:
+            }
+        }while(type != 3);
 
+        System.out.println("Bye bye!");
+        System.exit(0);
+
+    }
+
+        public static void printMenu () {
+
+            System.out.println("Hello, welcome to the calculator. \n");
+            System.out.println("1- F to C.");
+            System.out.println("2- C to F.");
+            System.out.println("3- Exit");
+            System.out.print("Please, type which option do you want: ");
 
         }
 
-    }
+        public static int valueReceiver () {
 
-    public static void printMenu(){
-
-        System.out.println("Hello, welcome to the calculator. \n");
-        System.out.println("1- F to C.");
-        System.out.println("2- C to F.");
-        System.out.println("3- Exit");
-        System.out.print("Please, type which option do you want:");
-
-    }
-
-    public static int valueReceiver(){
-
-        System.out.print("Ok, please type the value:");
-        int value = new Scanner(System.in).nextInt();
-        return value; //não consegui retornar direto
-    }
-
-    public static void result(Double value, int type){
-
-        switch (type) {
-            case 1:
-                System.out.printf("Your result was: %.2f Cº", value);
-                break;
-            case 2:
-                System.out.printf("Your result was: %.2f Fº", value);
-                break;
+            System.out.print("Ok, please type the value: ");
+            int value = new Scanner(System.in).nextInt();
+            return value; //não consegui retornar direto
         }
+
+        public static void result (Double value,int type){
+
+            switch (type) {
+                case 1:
+                    System.out.printf("Your result was: %.2f Cº \n", value);
+                    break;
+                case 2:
+                    System.out.printf("Your result was: %.2f Fº \n", value);
+                    break;
+                default:
+
+            }
+        }
+
+
+        public static Double convertCtoF ( double value){
+
+            return (value * 1.8) + 32;
+        }
+
+        public static Double convertFtoC ( double value){
+
+            return (value - 32) / 1.8;
+        }
+
+
     }
 
-
-    public static Double convertCtoF(double value){
-
-        return (value * 1.8) - 32;
-    }
-
-    public static Double convertFtoC(double value){
-
-        return (value * 1.8) - 32;
-    }
-
-
-
-}
