452c1e10053eb7b9adf95f36494cf3c219f34523
CarlosCardoso-prog <carloseduardocardsilva@gmail.com>
2021-01-17 22:47:54 -0300

    Java Cº to Fº Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,6 +13,10 @@ public class Calculator {
 
             value1 = (value1 * 1.8) + 32;
 
+            value1 = value1 * 100;
+            value1 = Math.round(value1);
+            value1 = value1 / 100;
+
             return value1;
 
     }
@@ -28,7 +32,13 @@ public class Calculator {
 
             value2 = (value2 - 32) / 1.8;
 
-            return value2;
+            value2 = value2 * 100;
+            value2 = Math.round(value2);
+            value2 = value2 / 100;
+            System.out.println("valor é " + value2);
+
+
+        return value2;
 
     }
 
@@ -36,51 +46,38 @@ public class Calculator {
 
         while(true) {
 
-            String type;
+            int type;
             Scanner Calc = new Scanner(System.in);
 
-            System.out.println("Hello, be welcome to the calculator. \n");
-            System.out.println("Please, type which type of value you want to insert: celsius or fahrenheit.");
-            type = Calc.nextLine();
-            type.toLowerCase();
+            System.out.println("Hello, welcome to the calculator. \n");
+            System.out.println("Please, type which option do you want:");
+            System.out.println("1- F to C. \n2- C to F. \n3- Exit");
+            type = Calc.nextInt();
 
-            if (type.equals("celsius")) {
+            if (type == 2) {
 
                 System.out.println("Converting Cº to Fº");
                 System.out.println("Please type the value");
                 double FinalResultC = CalculumToC();
                 System.out.println(FinalResultC + " Fº is the result");
-                System.out.println("Do you want to try again?");
-                String answer1 = Calc.nextLine();
-                answer1.toLowerCase();
 
-                if(answer1.equals("yes")){
-                    continue;
-                }else{
-                    System.out.println("Bye Bye");
-                    break;
-                }
+                    //continue
 
-            }else if(type.equals("fahrenheit")){
+            }else if(type == 1){
 
                 System.out.println("Converting Fº to Cº");
                 System.out.println("Please type the value");
                 double FinalResultF = CalculumToF();
                 System.out.println(FinalResultF + " Cº is the result");
-                System.out.println("Do you want to try again?");
-                String answer2 = Calc.nextLine();
-                answer2.toLowerCase();
 
-                if(answer2.equals("yes")){
-                    continue;
-                }else{
-                    System.out.println("Bye Bye");
-                    break;
-                }
+                    //continue
 
+            }else if(type == 3) {
+                System.out.println("Bye bye");
+                break;
             }else{
-                System.out.println("Please try again");
-                continue;
+                System.out.println("Sorry, couldn't understand.");
+                //continue
             }
 
         }
