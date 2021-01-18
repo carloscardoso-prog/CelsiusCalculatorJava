6da4881da7f362b542ab3a854ca4c5ed03ac33b0
CarlosCardoso-prog <carloseduardocardsilva@gmail.com>
2021-01-18 00:24:53 -0300

    Java Cº to Fº Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,87 +2,74 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static String CalculumToC(){
-        Scanner ValueReceiverC = new Scanner(System.in);
-
-        double value1;
-        String FV;
-
-        value1 = Double.parseDouble(ValueReceiverC.nextLine());
-
-        value1 = (value1 * 1.8) + 32;
-
-        value1 = value1 * 100;
-        value1 = Math.round(value1);
-        value1 = value1 / 100;
-        FV = String.valueOf(value1);
-        FV = FV.replace('.', ',');
-
-        return FV;
-
-    }
-
-    public static String CalculumToF(){
-        Scanner ValueReceiverF = new Scanner(System.in);
-
-        double value2;
-        String FV;
-
-        value2 = Double.parseDouble(ValueReceiverF.nextLine());
-
-        value2 = (value2 - 32) / 1.8;
-
-        value2 = value2 * 100;
-        value2 = Math.round(value2);
-        value2 = value2 / 100;
-        FV = String.valueOf(value2);
-        FV = FV.replace('.', ',');
-
-
-        return FV;
-
-    }
-
     public static void main(String[] args) {
 
-        while(true) {
+        printMenu();
 
-            int type;
-            Scanner Calc = new Scanner(System.in);
+        int type = new Scanner(System.in).nextInt();
+        int value = valueReceiver();
 
-            System.out.println("Hello, welcome to the calculator. \n");
-            System.out.println("Please, type which option do you want:");
-            System.out.println("1- F to C. \n2- C to F. \n3- Exit");
-            type = Calc.nextInt();
-
-            if (type == 2) {
-
-                System.out.println("Converting Cº to Fº");
-                System.out.println("Please type the value");
-                String FinalResultC = CalculumToC();
-                System.out.println(FinalResultC + " Fº is the result");
-
-                //continue
-
-            }else if(type == 1){
-
-                System.out.println("Converting Fº to Cº");
-                System.out.println("Please type the value");
-                String FinalResultF = CalculumToF();
-                System.out.println(FinalResultF + " Cº is the result");
-
-                //continue
-
-            }else if(type == 3) {
-                System.out.println("Bye bye");
+        switch(type){
+            case 1:
+                    result(convertFtoC(value), type);
                 break;
-            }else{
-                System.out.println("Sorry, couldn't understand.");
-                //continue
-            }
+
+            case 2:
+                result(convertCtoF(value), type);
+                break;
+
+            case 3:
+                System.out.println("Bye bye!");
+                System.exit(0);
+                break;
+
+            default:
+
 
         }
 
     }
+
+    public static void printMenu(){
+
+        System.out.println("Hello, welcome to the calculator. \n");
+        System.out.println("1- F to C.");
+        System.out.println("2- C to F.");
+        System.out.println("3- Exit");
+        System.out.print("Please, type which option do you want:");
+
+    }
+
+    public static int valueReceiver(){
+
+        System.out.print("Ok, please type the value:");
+        int value = new Scanner(System.in).nextInt();
+        return value; //não consegui retornar direto
+    }
+
+    public static void result(Double value, int type){
+
+        switch (type) {
+            case 1:
+                System.out.printf("Your result was: %.2f Cº", value);
+                break;
+            case 2:
+                System.out.printf("Your result was: %.2f Fº", value);
+                break;
+        }
+    }
+
+
+    public static Double convertCtoF(double value){
+
+        return (value * 1.8) - 32;
+    }
+
+    public static Double convertFtoC(double value){
+
+        return (value * 1.8) - 32;
+    }
+
+
 
 }
