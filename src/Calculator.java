3dbf90759d32623cd834ca20a09b6c2d885f0f03
CarlosCardoso-prog <carloseduardocardsilva@gmail.com>
2021-01-18 16:02:43 -0300

    Java Cº to Fº Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,83 +4,85 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+
+        while(true) {
+            calculator.printMenu();
+            calculator.readMenuInput();
+            }
+
+        }
+
+    private void printMenu () {
+
+        System.out.println("Hello, welcome to the calculator. \n");
+        System.out.println("Choose one of the options above: ");
+        System.out.println("1- F to C.");
+        System.out.println("2- C to F.");
+        System.out.println("3- Exit");
+        System.out.print("Please, type which option do you want: ");
+    }
+
+    private void readMenuInput(){
+
         int type;
 
-        do{
-            printMenu();
-
-            type = new Scanner(System.in).nextInt();
-
-            switch (type) {
-                case 1:
-                    double value = valueReceiver();
-                    System.out.println("Converting Fahrenheit to Celsius");
-                    result(convertFtoC(value), type);
-                    break;
-
-                case 2:
-                    value = valueReceiver();
-                    System.out.println("Converting Celsius to Fahrenheit");
-                    result(convertCtoF(value), type);
-                    break;
-
-                case 3:
-                    break;
-
-                default:
-                    System.out.println("Sorry, couldn't understand");
-                    continue;
-
+        while(true){
+            int menuOption = new Scanner(System.in).nextInt();
+            if(menuOption == 1 || menuOption == 2){
+                result(menuOption);
+                break;
             }
-        }while(type != 3);
+            if(menuOption == 3){
+                System.out.println("Bye bye!");
+                System.exit(0);
 
-        System.out.println("Bye bye!");
-        System.exit(0);
-
-    }
-
-        public static void printMenu () {
-
-            System.out.println("Hello, welcome to the calculator. \n");
-            System.out.println("1- F to C.");
-            System.out.println("2- C to F.");
-            System.out.println("3- Exit");
-            System.out.print("Please, type which option do you want: ");
-
-        }
-
-        public static int valueReceiver () {
-
-            System.out.print("Ok, please type the value: ");
-            int double = new Scanner(System.in).nextDouble();
-            return value; //não consegui retornar direto
-        }
-
-        public static void result (Double value,int type){
-
-            switch (type) {
-                case 1:
-                    System.out.printf("Your result was: %.2f Cº \n", value);
-                    break;
-                case 2:
-                    System.out.printf("Your result was: %.2f Fº \n", value);
-                    break;
-                default:
-
+            }else{
+                System.out.println("Sorry, couldn't understand");
+                break;
             }
         }
 
-
-        public static Double convertCtoF ( double value){
-
-            return (value * 1.8) + 32;
-        }
-
-        public static Double convertFtoC ( double value){
-
-            return (value - 32) / 1.8;
-        }
-
-
     }
+
+    private void result (int type){
+
+            double convertedValue = 0.0;
+            if(type == 1){
+                System.out.println("Converting Fahrenheit to Celsius");
+                convertedValue = convertCtoF(valueCalc());
+                System.out.printf("Result is %.2f Cº\n", convertedValue);
+            }
+            if(type == 2){
+                System.out.println("Converting Celsius to Fahrenheit");
+                convertedValue = convertFtoC(valueCalc());
+                System.out.printf("Result is %.2f Fº\n", convertedValue);
+            }
+        }
+
+    private double valueCalc(){
+        System.out.println("Please type the value");
+        String value = new Scanner(System.in).nextLine();
+
+        if(value.contains(",") && !value.contains(".")){
+            value = value.replace(",", ".");
+        }
+
+        return Double.parseDouble(value);
+    }
+
+
+    private Double convertCtoF (double value){
+
+        return (value * 1.8) + 32;
+    }
+
+
+    private Double convertFtoC (double value){
+
+        return (value - 32) / 1.8;
+    }
+
+
+}
 
